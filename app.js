ca3e0e3e345baf461c197edc1384ff259e1b92e9
Sagar Deshpande <101045627+Sagarkd2463@users.noreply.github.com>
2023-12-13 17:19:40 +0530

    Added files

---
+++ app.js
@@ -0,0 +1,96 @@
+if (process.env.NODE_ENV != 'production') {
+    require('dotenv').config();
+}
+
+const express = require('express');
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const flash = require('express-flash');
+const session = require('express-session');
+const methodOverride = require('method-override');
+
+const app = express();
+
+const users = [];
+const initializePassport = require('./passport-config')
+
+initializePassport(
+    passport,
+    email => {
+        users.find(user => user.email === email);
+    },
+    id => {
+        users.find(user => user.id === id);
+    }
+);
+
+app.set('view-engine', 'ejs');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(flash());
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride('_method'));
+
+app.get('/', checkAuthenticate, (req, res) => {
+    res.render('index.ejs', { name: req.user.name });
+});
+
+app.get('/login', checkNotAuthenticate, (req, res) => {
+    res.render('login.ejs');
+});
+
+app.get('/register', checkNotAuthenticate, (req, res) => {
+    res.render('register.ejs');
+});
+
+app.post('/register', checkNotAuthenticate, async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        });
+
+        res.redirect('/login');
+
+    } catch (err) {
+        res.redirect('/register');
+    }
+});
+
+app.post('/login', checkNotAuthenticate, passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}));
+
+app.delete('/logout', (req, res) => {
+    req.logOut();
+    res.redirect('/login');
+});
+
+function checkAuthenticate(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+
+    res.redirect('/login');
+}
+
+function checkNotAuthenticate(req, res, next) {
+    if(req.isAuthenticated()) {
+        return res.redirect('/');
+    }
+    next();
+}
+
+app.listen(3000);
